f3c053461d20033e2dd1cfb924a0c047368e2f25
Pragyaditya Das <Pragyaditya Das>
2017-02-20 23:07:02 +0530

    Very very simple

---
+++ Prob6.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+
+	int i,j,y,sum1,sum2;
+	
+  	int sum=0;
+
+  	for (i=1;i<101;i++){
+    	sum+=i*i;
+ 		 }
+ 		 
+  	y=0;
+  	
+  	for (j=1;j<101;j++){
+    		y=y+j;
+  		}
+  	sum2=y*y;
+  
+  	printf("Answer: %dn",sum2-sum);
+
+  return 0;
+
+}
